3f3b7ab91e2855981b79d44c5b99809e631b63a2
Ефимов Максим <efimov_m@itstep.academy>
2021-06-15 19:18:36 +0300

    add swap function

--- Darray/Source.cpp
+++ Darray/Source.cpp
@@ -13,22 +13,22 @@ int main()
 {
 	Darray<int> darr(10);
 
-	darr.Print();
+	/*darr.Print();
 	darr.RandomInit(10, 120);
 	darr.Print();
 	darr.Insert(5, 200);
 	darr.At(2) = 500;
 	darr[3] = 600;
-	darr.Print();
+	darr.Print();*/
 
-	/*int size{ 10 };
+	int size{ 10 };
 	int* array = new int[size];
 	arrayInit(array, size);
 	arrayPrint(array, size);
 	arrayInsert(array, size, 5, 100);
 	arrayPrint(array, size);
 	arrayRemove(array, size, 7);
-	arrayPrint(array, size);*/
+	arrayPrint(array, size);
 	return 0;
 }
 
@@ -73,4 +73,11 @@ void arrayRemove(int*& array, int& size, int index)
 	array = temp;
 
 	size--;
+}
+
+void myswap(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
 }
